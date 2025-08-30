5dd0785e2768721bd0a2bd8fbb842797431dcd47
Mohd Daniyaz Ali <99903535+daniyaz0786@users.noreply.github.com>
2025-08-30 16:42:50 +0530

    Update userSignIn.js

--- controller/userSignIn.js
+++ controller/userSignIn.js
@@ -6,51 +6,36 @@ async function userSignInController(req, res) {
   try {
     const { email, password } = req.body;
 
-    if (!email) {
-      throw new Error("Please Provide email");
-    }
-    if (!password) {
-      throw new Error("Please Provide password");
-    }
+    if (!email) throw new Error("Please Provide email");
+    if (!password) throw new Error("Please Provide password");
 
     const user = await userModel.findOne({ email });
-    if (!user) {
-      throw new Error("User Not Found!");
-    }
+    if (!user) throw new Error("User Not Found!");
 
     const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) throw new Error("Password Doesn't Match!");
 
-    // console.log("checkPassword", checkPassword);
-
-    // If the password is incorrect
-    if (!checkPassword) {
-      throw new Error("Password Doesn't Match!");
-    }
-
-    // If the password is correct, generate a token
-    const tokenData = {
-      _id: user._id,
-      email: user.email,
-    };
+    const tokenData = { _id: user._id, email: user.email };
     const token = jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, {
-      expiresIn: 60 * 60 * 8, // Token expiration time
+      expiresIn: "8h", // 8 hours
     });
-
-    // console.log("Generated Token:", token);
 
     const tokenOption = {
       httpOnly: true,
-      secure: true, // Ensure secure cookie in production
+      secure: process.env.NODE_ENV === "production", 
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 8 * 60 * 60 * 1000, // 8 hours
     };
+
     res.cookie("token", token, tokenOption).json({
       message: "Login Successfully",
-      data: token,
+      data: { token, userId: user._id },
       success: true,
       error: false,
     });
   } catch (err) {
     res.status(500).json({
-      message: err.message || err,
+      message: err.message || "Login failed",
       error: true,
       success: false,
     });
